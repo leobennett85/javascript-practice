dbeb2e29358f0811bb8a25541e798663c6d48364
leobennett85 <103042120+leobennett85@users.noreply.github.com>
2022-05-03 15:54:55 -0230

    01-HelloWorld

---
+++ modules/Fundamentals/05-DataTypes/js.js
@@ -0,0 +1,20 @@
+let message = 12.5;
+message = "Hello";
+
+alert("Used to say hello now says: " + message);
+
+//Strings and backticks
+let name = "John";
+ //Use backticks to embed a variable
+ alert(`Hello, ${name}!`);
+ //You can also embed expressions
+ alert(`Hello, ${1+2}!`);
+
+ //Booleans
+ let nameFieldChecked = "true";
+ let ageFieldChecked= "false";
+
+ let isGreater = 4 > 1; //returns true
+ alert(isGreater);
+
+ alert(typeof(message));
