f3ebc8ac25478b95098caa5a84d1ca6d620b574d
leobennett85 <103042120+leobennett85@users.noreply.github.com>
2022-05-09 14:34:00 -0230

    06-Interaction

---
+++ modules/Fundamentals/06-Interaction/js.js
@@ -0,0 +1,4 @@
+let result = prompt('PROMPT: How old are you?', 100);
+alert(`ALERT: You are ${result} years old.`);
+let result2 = confirm('CONFIRM: Ture or False?');
+alert(`ALERT: You are ${result2}.`);
