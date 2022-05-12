9750e2c7ccbc1e4d5f972e22cdc482f3465cf96d
leobennett85 <103042120+leobennett85@users.noreply.github.com>
2022-05-12 18:17:33 -0230

    09-Comparisons

---
+++ modules/Fundamentals/09-Comparisons/js.js
@@ -0,0 +1,11 @@
+let a = 5>4;
+let b = "apple" > "pineapple";
+let c = "2" > "12";
+let d = undefined == null;
+let e = undefined === null;
+
+document.write(`5>4: ${a}`)
+document.write(`apple > pineapple: ${b}`);
+document.write(`'2' > '12': ${c}`);
+document.write(`undefined == null: ${d}`);
+document.write(`undefined === null: ${e}`);
